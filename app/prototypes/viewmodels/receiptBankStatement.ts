702eaa9384925c46b935b45a40bc949790bff278
Lee crosby <leemcrosby@googlemail.com>
2017-06-01 19:21:07 +0100

    receipt bank statement page

--- app/prototypes/viewmodels/receiptBankStatement.ts
+++ app/prototypes/viewmodels/receiptBankStatement.ts
@@ -9,7 +9,7 @@ import * as moment from 'moment';
 
 
 /**
- * MoneyDueIn VM
+ * ReceiptBankStatement VM
  */
 
 function Employee ( name, position, salary, office ) {
@@ -40,35 +40,43 @@ enum SelectedMode {
   bankStatement
 }
 
-class MoneyDueIn {
+class ReceiptBankStatement {
     public isLoading:KnockoutObservable<boolean> = ko.observable(false);
     public table;
     public searchText:KnockoutObservable<string> = ko.observable("");
     public data: KnockoutObservableArray<any> = ko.observableArray([]);
+    public bankStatementData: KnockoutObservableArray<any> = ko.observableArray([]);
     public basket: KnockoutObservableArray<Receipts> = ko.observableArray([]);
     public processEnum = ProcessingState;
     public modeEnum = SelectedMode;
     public receiptsProcessingState: KnockoutObservable<any> = ko.observable(ProcessingState.notStarted);
     public uploadFileProcessingState: KnockoutObservable<any> = ko.observable(ProcessingState.notStarted);
     public selectedModeState: KnockoutObservable<any> = ko.observable(SelectedMode.manual);
+    public selectedItem: KnockoutObservable<any> = ko.observable(null);
+    public processReceiptsDisabled: KnockoutComputed<boolean>;
 
     activate() {
-      let self:MoneyDueIn = this;
+      let self:ReceiptBankStatement = this;
         this.isLoading(true);
         globalui.instance.hideMenu(true);
         this.searchText.subscribe((value)=> {
           console.log(value);
-          self.table.search(value).draw();
-
-          //reapply ko bindings
-          ko.cleanNode(document.getElementById("main-table"));
-          ko.applyBindings(self, document.getElementById("main-table"));
+          self.searchTable(value);
         });
+
+        this.selectedItem.subscribe((value) => {
+            self.updateTable();
+            self.searchForItem(value);
+        });
+
+        this.processReceiptsDisabled = ko.computed(() => {
+            return this.confirmedItemCount() == 0;
+        }, this);
     }
 
     compositionComplete() {
-
-        this.getData('/dist/data/moneyDueIn.json').then((data) => {
+        let self: ReceiptBankStatement = this;
+        this.getData('/dist/data/MoneyDueIn.json').then((data) => {
           this.data(data);
           console.log(this.data());
           //create table
@@ -113,18 +121,38 @@ class MoneyDueIn {
                 { data: 'total' },
                 { data: 'paid' },
                 { render: function(data, type, full, meta){
-                  return `
-                      <form class="form-inline row">
-                        <div class="form-group  col-xs-8">
-                          <label class="sr-only" for="exampleInputAmount">Amount</label>
-                          <div class="input-group">
-                            <input type="text" class="form-control input-sm" id="pay-input-${meta.row}" value="${(Math.round((full.total - full.paid)*100)/100)}" placeholder="Amount">
-                          </div>
-                        </div>
-                        <div class="row">
-                          <button type="submit" class="btn btn-success btn-sm" data-bind="click: addReceipt.bind(this, ${meta.row})">Add</button>
-                        </div>
-                      </form>`;
+                        let selectedReference = '';
+                        
+                        if(self.selectedItem()){
+                            if(self.selectedItem().matchingInvoice){
+                                selectedReference = self.selectedItem().matchingInvoice.reference;
+                            }
+                        }
+                        console.log(selectedReference);
+                        let previouslyAllocated = false;
+
+                        self.bankStatementData().forEach(item => {
+                            if(item.matchingInvoice && item.matchingInvoice.reference == full.reference){
+                                previouslyAllocated = true;
+                            }
+                        });
+
+                        if(!previouslyAllocated || selectedReference == full.reference){
+                            switch(full.status) {
+                                case 'auto-allocated':
+                                return `<button type="submit" class="btn btn-success btn-sm" data-bind="click: setStatus.bind(this, ${meta.row}, 'confirmed')">Confirm</button> <button type="submit" class="btn btn-default btn-sm" data-bind="click: setStatus.bind(this, ${meta.row}, 'unallocated')">Reallocate</button>`;
+
+                                case 'confirmed':
+                                return `<span>Confirmed</span> <a data-bind="click: setStatus.bind(this, ${meta.row}, 'unallocated')">undo</a>`
+
+                                default:
+                                return `<button type="submit" class="btn btn-default btn-sm" data-bind="click: setStatus.bind(this, ${meta.row}, 'confirmed')">Allocate</button>`;
+                            }
+                        }
+                        else{
+                            return 'Previously allocated';
+                        }
+                    
                     }
                 },
                 { data: 'notes', render: function(data){
@@ -134,20 +162,93 @@ class MoneyDueIn {
           });
           
           ko.applyBindings(this, document.getElementById("main-table"));
+
+          
+            this.getData('/dist/data/bankStatement.json').then((data) => {
+                data.forEach(statementItem => {
+                    let matched = false;
+                    this.data().forEach(dueItem => {
+                        if(dueItem.status != 'auto-allocated'){
+                            if(dueItem.reference == statementItem.reference){
+                                matched = true;
+                                dueItem.status = 'auto-allocated';
+                                statementItem.matchingInvoice = dueItem;
+                            }
+                            else{
+                                dueItem.status = 'unallocated';
+                            }
+                        }
+                    });
+                    statementItem.status = ko.observable(matched ? "auto-allocated" : "unallocated");
+                });
+                this.bankStatementData(data);
+                this.selectedItem(this.bankStatementData()[0]);
+            });
         });
 
       
     }
     
+    public confirmedItemCount():number {
+        let count:number = 0;
+
+        this.bankStatementData().forEach((item) => {
+            console.log("processReceiptsDisabled>>>>>>>>>>>>>>");
+            console.log(item.status());
+            if(item.status() == 'confirmed'){
+                count++;
+            }
+        });
+
+        return count;
+    }
+
+    public setStatus(index:number, status:string, model:ReceiptBankStatement, event:JQueryEventObject) {
+        let index:number;
+        model.data()[index].status = status;
+        model.selectedItem().status(status);
+        model.selectedItem().matchingInvoice = model.data()[index];
+        console.log(model.selectedItem());
+        console.log(model.bankStatementData());
+        if(status == "confirmed"){
+            index = model.bankStatementData().findIndex((element) => {
+                return element == model.selectedItem();
+            });
+            model.selectedItem(model.bankStatementData()[index + 1]);
+        }
+        model.updateTable();
+        model.searchForItem(model.selectedItem());
+    }
+
+    public searchTable(value:string) {
+        let self: ReceiptBankStatement = this;
+        self.table.search(value).draw();
+
+        //reapply ko bindings
+        ko.cleanNode(document.getElementById("main-table"));
+        ko.applyBindings(self, document.getElementById("main-table"));
+    }
+
+    public searchForItem(item) {
+        let self: ReceiptBankStatement = this;
+        console.log("search for item");
+        self.searchText(item.matchingInvoice ? item.matchingInvoice.reference : item.reference);
+    }
+
+    public selectItem(item, event){
+        let self: ReceiptBankStatement = this;
+        self.selectedItem(item);
+    }
+
     public setSelectedBankStatementMode() {
-      let self: MoneyDueIn = this;
+      let self: ReceiptBankStatement = this;
       self.selectedModeState(SelectedMode.bankStatement);
       
       $('#upload-bank-statement-modal').modal('hide');
     }
 
     public selectFile() {
-      let self: MoneyDueIn = this;
+      let self: ReceiptBankStatement = this;
 
       this.uploadFileProcessingState(ProcessingState.started);
 
@@ -163,7 +264,7 @@ class MoneyDueIn {
     }
 
     public confirmProcessing() {
-      let self: MoneyDueIn = this;
+      let self: ReceiptBankStatement = this;
 
       this.receiptsProcessingState(ProcessingState.started);
 
@@ -185,7 +286,7 @@ class MoneyDueIn {
       console.log("save receipts");
     }
 
-    public addReceipt(index:number, model:MoneyDueIn, event:JQueryEventObject) {
+    public addReceipt(index:number, model:ReceiptBankStatement, event:JQueryEventObject) {
       let amount:number = parseFloat($(document.getElementById(`pay-input-${index}`)).val());
       let invoice = model.data()[index];
       console.log(model.data());
@@ -203,7 +304,7 @@ class MoneyDueIn {
     }
 
     public removeReceipt(item: Receipts,event: JQueryEventObject){
-      let self: MoneyDueIn = this;
+      let self: ReceiptBankStatement = this;
       item.invoice.paid = (Math.round((item.invoice.paid - item.amount)*100)/100);
       self.data.push(item.invoice);
       self.basket.splice(self.basket().indexOf(item), 1);
@@ -211,7 +312,7 @@ class MoneyDueIn {
     }
 
     public updateTable(){
-      let self: MoneyDueIn = this;
+      let self: ReceiptBankStatement = this;
       //update data model - filter out row if balance paid
       self.data(self.data().filter(function(item){
         return (Math.round((item.total - item.paid)*100)/100) > 0;
@@ -250,4 +351,4 @@ class MoneyDueIn {
 
 }
 
-export = MoneyDueIn;
+export = ReceiptBankStatement;
--- app/viewmodels/shell.ts
+++ app/viewmodels/shell.ts
@@ -9,13 +9,14 @@ public ui = global.instance;
 
   activate() {
     router.map([
-      { route: '',                      moduleId: 'viewmodels/home',                              title: "Home",                  nav: true },
-      { route: 'components',            moduleId: 'viewmodels/components',                        title: "Components",            nav: true },
-      { route: 'structures',            moduleId: 'viewmodels/structures',                        title: "Structures",            nav: true },
-      { route: 'templates',             moduleId: 'viewmodels/templates',                         title: "Templates",             nav: true },
-      { route: 'prototypes',            moduleId: 'viewmodels/prototypes',                        title: "Prototypes",            nav: true },
-      { route: 'accountingDashboard',   moduleId: '../prototypes/viewmodels/accountingDashboard', title: "Test",                  nav: false },
-      { route: 'moneyDueIn',            moduleId: '../prototypes/viewmodels/moneyDueIn',          title: "Test",                  nav: false }
+      { route: '',                                moduleId: 'viewmodels/home',                                        title: "Home",                  nav: true },
+      { route: 'components',                      moduleId: 'viewmodels/components',                                  title: "Components",            nav: true },
+      { route: 'structures',                      moduleId: 'viewmodels/structures',                                  title: "Structures",            nav: true },
+      { route: 'templates',                       moduleId: 'viewmodels/templates',                                   title: "Templates",             nav: true },
+      { route: 'prototypes',                      moduleId: 'viewmodels/prototypes',                                  title: "Prototypes",            nav: true },
+      { route: 'accountingDashboard',             moduleId: '../prototypes/viewmodels/accountingDashboard',           title: "Test",                  nav: false },
+      { route: 'moneyDueIn',                      moduleId: '../prototypes/viewmodels/moneyDueIn',                    title: "Test",                  nav: false },
+      { route: 'receiptBankStatement',            moduleId: '../prototypes/viewmodels/receiptBankStatement',          title: "Test",                  nav: false }
     ]).buildNavigationModel();
     return router.activate();
   };
