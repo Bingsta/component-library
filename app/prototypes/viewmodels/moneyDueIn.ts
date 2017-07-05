19332d59e79d574bded68c0dedaa840e71915447
bingsta <leemcrosby@googlemail.com>
2017-07-05 17:58:34 +0100

    moneyduein amendments

--- app/prototypes/viewmodels/moneyDueIn.ts
+++ app/prototypes/viewmodels/moneyDueIn.ts
@@ -44,6 +44,8 @@ enum SelectedMode {
 class MoneyDueIn {
     public isLoading:KnockoutObservable<boolean> = ko.observable(false);
     public table;
+    public el_table;
+    public el_tableContainer;
     public searchText:KnockoutObservable<string> = ko.observable("");
     public data: KnockoutObservableArray<any> = ko.observableArray([]);
     public basket: KnockoutObservableArray<Receipts> = ko.observableArray([]);
@@ -76,59 +78,86 @@ class MoneyDueIn {
         this.getData('/dist/data/moneyDueIn.json').then((data) => {
           this.data(data);
 
-          let tableContainer = document.getElementById("app-main-table__container"),
-          table = document.getElementById("example");
+          this.el_tableContainer = document.getElementById("app-main-table__container"),
+          this.el_table = document.getElementById("example");
 
-          console.log($(tableContainer).height());
-          this.table = $(table).DataTable( {
-              scrollY:        ($(tableContainer).height()-41),
+          console.log($(this.el_tableContainer).height());
+          this.table = $(this.el_table).DataTable( {
+              scrollY:        ($(this.el_tableContainer).height()-41),
               scrollX:        true,
               scrollCollapse: true,
               paging:         false,
               searching:      false,
               info:           false,
               data:           this.data(),
+              drawCallback: () => {
+              },
+              fixedHeader:    true,
+              createdRow: ( row, data, index ) => {
+                if(parseFloat(data.arrearsTotal) > 0) {
+                  $(row).addClass('danger');
+                }
+              },
               columns: [
                 { name: 'reference',  data: 'reference' },
-                { name: 'owed_by',    data: 'owed_by'},
                 { name: 'total',      data: "total" },
-                { name: 'bal',        data: 'balance'},
-                { name: 'type',       data: 'type',
-                  render: function(data) {
-                      switch(data){
-                        case "Rent demand":
-                        return `<span class="label label-warning" data-toggle="tooltip" data-placement="top" title="${data}">RD</span>`;
-                        case "Work order":
-                        return `<span class="label label-info" data-toggle="tooltip" data-placement="top" title="Invoice">INV</span>`;
-                        default:
-                          return data;
-                      }
+                { name: 'regarding',  data: 'regarding'},
+                { name: 'owed_by',    data: 'owed_by',
+                  render: (data) => {
+                    return `<a class="contact__item" href="javascript: void(0)">${data}</a>`
                   }
                 },
-                { name: 'regarding',  data: 'regarding'},
                 { name: 'due_on',     data: 'due_on',         type:"date" },
+                { name: 'bal',        data: 'balance',
+                  render: (data) => {
+                    return `<a class="account__balance" href="javascript: void(0)">${data}</a>`
+                  } 
+                },
+                {
+                  name: 'arrears',    data: 'arrearsTotal',
+                  render: (data, type, full, meta) => {
+                    if(parseFloat(data) > 0) {
+                      return `
+                      <div class="grid grid--padded-xs">
+                        <div class="grid__item">
+                          <a  class="account__balance" href="javascript: void(0)">${data}</a>
+                        </div>
+                        <div class="grid__item">
+                          <span class="badge">${full.arrearsCount}</span>
+                        </div>
+                      </div>`
+                    }
+                    else {
+                      return `0.00`;
+                    }
+                  }
+                },
+                { name: 'use_bal',    data: 'balance',        orderable: false,       className: 'table__fixed-col',
+                  render: (data, type, full, meta) => {
+                    return `
+                          <input type="text" style="width:6rem" class="form-control input-xs" placeholder="Amount" id="use-bal-input-${meta.row}" value="${full.balance}">
+                        `
+                  }
+                },
                 { name: 'amounts',    data: 'paid',           orderable: false,       className: 'table__fixed-col',
-                  render: () => {
+                  render: (data, type, full, meta) => {
                       return `<form class="grid  grid--padded-xs">
                                 <div class="grid__item">
-                                  <input type="text" style="width:8rem" class="form-control input-xs" placeholder="Amount" data-bind="attr: {id: 'use-bal-input-' + $index() }, value: balance">
-                                </div>
-                                <div class="grid__item">
-                                  <input type="text" style="width:8rem" class="form-control input-xs" placeholder="Amount" data-bind="attr: {id: 'receipt-input-' + $index() }, value: (Math.round((total - paid)*100)/100)">
+                                  <input type="text" style="width:6rem" class="form-control input-xs" placeholder="Amount" id="receipt-input-${meta.row}" value="${(Math.round((full.total - full.paid)*100)/100)}">
                                 </div>
                                 <div class="grid__item grid__item--fill">
 
                                   <div class="btn-group">
-                                    <button type="button" class="btn btn-success btn-xs" data-bind="click:$parent.addReceipt.bind($parent, $index)">Receipt&nbsp;&nbsp;<i class="icon-arrow-right2"></i></button>
+                                    <button type="button" data-rowIndex="${meta.row}" class="btn btn-success btn-xs  receipt__button">Receipt&nbsp;&nbsp;<i class="icon-arrow-right2"></i></button>
                                     <button type="button" class="btn btn-success btn-xs dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                       <span class="caret"></span>
                                       <span class="sr-only">Toggle Dropdown</span>
                                     </button>
                                     <ul class="dropdown-menu dropdown-menu-right">
-                                      <li><a href="#">Receipt and pay landlord</a></li>
+                                      <li><a class="receipt_and_pay" href="javascript:void(0)">Receipt and pay landlord</a></li>
                                       <li role="separator" class="divider"></li>
-                                      <li><a href="#">Receipt by cash</a></li>
-                                      <li><a href="#">Receipt by check</a></li>
+                                      <li><a class="receipt_and_pay" href="javascript:void(0)">Receipt by cash</a></li>
+                                      <li><a class="receipt_and_pay" href="javascript:void(0)">Receipt by check</a></li>
                                     </ul>
                                   </div>
                                   
@@ -141,12 +170,12 @@ class MoneyDueIn {
                   console.log(data);
                   switch(data > 0) {
                     case true:
-                      return `<div class="iconCount iconCount-accent">
+                      return `<div class="iconCount iconCount-accent  account__notes">
                                 <i class="iconCount__icon icon icon-bubble3"></i>
                                 <span class="iconCount__count">${data}</span>
                               </div>`;
                     case false:
-                      return `<div class="iconCount">
+                      return `<div class="iconCount  account__notes">
                                 <i class="iconCount__icon icon icon-bubble3"></i>
                                 <span class="iconCount__count"></span>
                               </div>`
@@ -156,102 +185,54 @@ class MoneyDueIn {
                },
                 { name: 'options',    data: null,             orderable: false,       className: 'table__fixed-col', 
                   render: () => {
-                    return `<button class="btn btn-circle btn-sm btn-default"><i class="icon icon-menu"></i></button>`;
+                    return `
+                    <div class="dropdown">
+                      <button class="btn btn-circle btn-sm btn-default" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="icon icon-menu"></i></button>
+                      <ul class="dropdown-menu dropdown-menu-right">
+                        <li><a href="javascript:void(0)">Print invoice</a></li>
+                        <li><a href="javascript:void(0)">Email tenant</a></li>
+                        <li><a href="javascript:void(0)">Email landlord</a></li>
+                      </ul>
+                    </div>`;
                   } 
                 }
-              ]
+              ],
               fixedColumns: {
-                rightColumns:   3,
+                rightColumns:   4,
                 leftColumns:    0
               }
-          } );
+          });
+    
+          this.bindClickHandlers();
+          //bind jquery click handler
 
-          //create table
-          // this.table = $("#main-table").DataTable({
-          //   fixedColumns:   true,
-          //   searching:      true,
-          //   paging:         false,
-          //   data: this.data().filter(function(item){
-          //     return (Math.round((item.total - item.paid)*100)/100) > 0;
-          //   }),
-          //   order: [[9, "desc"]],
-          //   dom: "t",
-          //   columns: [
-          //       { data: 'reference' },
-          //       { data: 'owed_by', render: function(data) {
-          //         if(data.length > 15){
-          //           return `<span data-toggle="tooltip" data-placement="top" title="${data}">${data.substr(0, 10) + "..."}</span>`;
-          //         }
-          //         return data;
-          //       } },
-          //       { data: 'total' },
-          //       { data: 'paid' },
-          //       { data: 'balance' },
-          //       { render: function(data, type, full, meta){
-          //         return `<input type="text" class="form-control input-xs" id="use-bal-input-${meta.row}" value="${full.balance}" placeholder="Amount">
-          //                 `;
-          //           }
-          //       },
-          //       { render: function(data, type, full, meta){
-          //         return `
-          //             <form class="grid  grid--padded-sm">
-          //               <div class="grid__item">
-          //                 <input type="text" class="form-control input-xs" id="receipt-input-${meta.row}" value="${(Math.round((full.total - full.paid)*100)/100)}" placeholder="Amount">
-          //               </div>
-          //               <div class="grid__item">
-          //                 <button type="submit" class="btn btn-success btn-xs" data-bind="click: addReceipt.bind(this, ${meta.row})">Receipt</button>
-          //               </div>
-          //             </form>`;
-          //           }
-          //       },
-          //       { data: 'type', render: function(data) {
-          //           switch(data){
-          //             case "Rent demand":
-          //             return `<span class="label label-warning" data-toggle="tooltip" data-placement="top" title="${data}">RD</span>`;
-          //             case "Work order":
-          //             return `<span class="label label-info" data-toggle="tooltip" data-placement="top" title="Invoice">INV</span>`;
-          //             default:
-          //               return data;
-          //           }
-          //       } },
-          //       { data: 'regarding', render: function(data) {
-          //         if(data.length > 20){
-          //           return `<span data-toggle="tooltip" data-placement="top" title="${data}">${data.substr(0, 10) + "..."}</span>`;
-          //         }
-          //         return data;
-          //       } },
-          //       { type: "date", data: 'due_on' },
-          //       { data: 'pay_to', render: function(data) {
-          //         if(data.length > 15){
-          //           return `<span data-toggle="tooltip" data-placement="top" title="${data}">${data.substr(0, 10) + "..."}</span>`;
-          //         }
-          //         return data;
-          //       } },
-          //       { data: 'notes', render: function(data){
-          //         return data? `<span class="badge badge-accent">${data}</span>` : '';
-          //       } }
-          //   ]
-          // });
-
-          // this.refreshTable()
-          // 
         });
 
       
     }
     
-    public refreshTable() {
-      let self: MoneyDueIn = this;
-      console.log("refresh table");
-      let table = $("#main-table");
-      let tHeadTHs = table.find(">thead>tr>th");
-      let tHead = table.find(">")
-      table.find(">tbody").scroll((event) => {
-        table.find(">thead").scrollLeft(event.currentTarget.scrollLeft);
-      });
-      table.find(">tbody>tr:first-child>td").each((index, value) => {
-        $(value).css({minWidth : $(tHeadTHs[index]).outerWidth()});
-      });
+    public bindClickHandlers() {
+      $('.receipt__button').click(this.addReceipt.bind(this));
+      $('.contact__item').click(this.showContact.bind(this));
+      $('.account__balance').click(this.showAccountStatement.bind(this));
+      $('.account__notes').click(this.showAccountNotes.bind(this));
+      $('.receipt_and_pay').click(this.receiptAndPay.bind(this));
+    }
+
+    public showContact() {
+      alert("show contact details");
+    }
+
+    public showAccountStatement() {
+      alert("show account statement");
+    }
+
+    public showAccountNotes() {
+      alert("show account notes stream");
+    }
+
+    public receiptAndPay() {
+      alert("Show receipt and pay interface");
     }
 
     public selectFile() {
@@ -294,25 +275,24 @@ class MoneyDueIn {
       console.log("save receipts");
     }
 
-    public addReceipt(index, invoice, event) {
+    public addReceipt(event: JQueryEventObject) {
+      console.log(event);
 
-      let amountReceipted:number = parseFloat($(document.getElementById(`receipt-input-${index()}`)).val() as string);
-      let useBalAmount:number = parseFloat($(document.getElementById(`use-bal-input-${index()}`)).val() as string);
+      var index = event.currentTarget.getAttribute('data-rowIndex');
+      console.log(this);
+      let amountReceipted:number = parseFloat($(document.getElementById(`receipt-input-${index}`)).val() as string);
+      let useBalAmount:number = parseFloat($(document.getElementById(`use-bal-input-${index}`)).val() as string);
+      let invoice = this.data()[index];
 
       invoice.paid = (Math.round((parseFloat(invoice.paid) + amountReceipted + useBalAmount)*100)/100);
+      
       this.basket.push({
-        id: invoice.reference,
+        id: invoice.owed_by,
         payee: invoice.owed_by,
         amount: amountReceipted,
         invoice: invoice
       });
-      console.log(this.data().length);
-      this.data.remove(invoice);
-      ko.cleanNode(document.getElementById("example"));
-      ko.applyBindings({}, document.getElementById("example"));
-         
-      console.log(this.data().length);
-      //this.updateTable();
+      this.updateTable();
     }
 
     public showReceiptDetails(model, event) {
@@ -331,22 +311,25 @@ class MoneyDueIn {
       let self: MoneyDueIn = this;
       //update data model - filter out row if balance paid
       console.log(self.data().length)
-      self.data(self.data().filter(function(item){
+      self.data(self.table.rows().data().filter(function(item){
         return (Math.round((item.total - item.paid)*100)/100) > 0;
       }))
       
       console.log(self.data().length)
       // self.searchText("");
 
-      // //update table and data model
-      // self.table.clear();
-      // self.table.rows.add(self.data());
-      // self.table.draw();
+      //update table and data model
+      self.table.clear();
 
-      // //reapply ko bindings
-      // ko.cleanNode(document.getElementById("main-table"));
-      // ko.applyBindings(self, document.getElementById("main-table"));
-      // self.refreshTable()
+      self.table.rows.add(self.data());
+      self.table.draw();
+      // self.table.fixedColumns().update();
+      
+      // self.table.fixedColumns().relayout();
+      
+      console.log(self.el_tableContainer);
+      
+      this.bindClickHandlers();
     }
 
     public searchData(model, event: JQueryEventObject) {
